4d2a734ccf39bf0ee05faad7579cd39026ff9dee
Dmytro Tiulpa <dmytrotiulpa@gmail.com>
2023-11-30 08:11:31 +0200

    vue install

---
+++ resources/js/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Hello, {{ username }}!</h1>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+    components: {},
+    setup() {
+        return {
+            username: 'DiMoN'
+        };
+    },
+});
+
+</script>
+
+<style scoped>
+
+</style>
